77bbdc7901355715ef24bcef2406c7a0a1454060
WeltAine <1352410661@qq.com>
2026-02-18 17:19:01 +0800

    调整构建文件

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,9 @@ project "ImGui"
     location "ImGui"
     kind "StaticLib"
     language "C++"
+    cppdialect "C++20"
+    staticruntime "On"
+
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -20,10 +23,10 @@ project "ImGui"
         "imgui_demo.cpp"
     }
 
-    filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++20"
-        staticruntime "On"
+    filter "configurations:Debug" --暂时没用，但先写着
+        runtime "Debug" --使用调试版本的运行时库（比如msvcrtd.lib），以便在调试时能够获得更详细的错误信息和调试支持
+        symbols "On" 
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Release"
+        runtime "Release" --使用发布版本的运行时库（比如msvcrt.lib），以获得更好的性能和较小的二进制文件
+        optimize "On"
